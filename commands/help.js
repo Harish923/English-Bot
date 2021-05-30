76bc927ab93a5f689b8b8d47c6332132f8569ea4
Harish923 <ramagii.hari9@gmail.com>
2021-05-30 10:00:16 +0530

    updating help.js to show new !disconnect

--- commands/help.js
+++ commands/help.js
@@ -4,9 +4,7 @@ module.exports = function(msg, args) {
     const embed = new Discord.MessageEmbed(msg)
     .setTitle('English Bot Help')
     .setColor('#9933ff')
-    .setDescription(`!meaning [word]: returns meaning of word\n
-                    !say [word]: says the word in the voice channel you joined\n
-                    !allabout [word]: returns meaning and part of speech of the word`);
+    .setDescription("!meaning [word]: returns meaning of word\n!say [word]: says the word in the voice channel you joined\n!allabout [word]: returns meaning and part of speech of the word\n!disconnect: to disconnect bot from a voice channel");
     
     msg.channel.send(embed);
 }
